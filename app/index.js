d02e818ff36e209df8e122de5f70c01401b3174b
Ayush Senapati <ayushsenapati123@gmail.com>
2023-03-19 21:51:13 +0530

    Home function

---
+++ app/index.js
@@ -0,0 +1,12 @@
+// View is similar to <div> and Text is similar to <p>
+import { View, Text } from "react-native";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home...</Text>
+        </View>
+    )
+}
+
+export default Home;
