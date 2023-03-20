34edbcf507f99232f2a75021b6adc7fe93e0dcf3
Ayush Senapati <ayushsenapati123@gmail.com>
2023-03-20 14:33:40 +0530

    Updated Home page

--- app/index.js
+++ app/index.js
@@ -1,12 +1,49 @@
 // View is similar to <div> and Text is similar to <p>
-import { View, Text } from "react-native";
+//! This will be our HOME page
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
-    return (
-        <View>
-            <Text>Home...</Text>
+  const router = useRouter(); // using useRouter hook.
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          //   headerTitle: "Ayush"
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+            <Welcome />
         </View>
-    )
-}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
