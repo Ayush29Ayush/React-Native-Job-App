0cd80439d9bb1a44aad6e62d2bca988e9c7d974d
Ayush Senapati <ayushsenapati123@gmail.com>
2023-03-29 14:26:02 +0530

    Updated job details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,9 +1,38 @@
-import { Text } from "react-native"
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+import { COLORS, icons, SIZES } from "../../constants";
+import useFetch from "../../hook/useFetch";
 
 const JobDetails = () => {
-  return (
-    <Text>JobDetails</Text>
-  )
-}
+  const params = useSearchParams();
+  const router = useRouter();
 
-export default JobDetails
+  const { data, isLoading, error, refetch } = useFetch("job-details", {
+    job_id: params.id,
+  });
+
+  return (
+    <SafeAreaView>
+
+    </SafeAreaView>
+  )
+};
+
+export default JobDetails;
